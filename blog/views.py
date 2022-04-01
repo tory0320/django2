d91cef4a0eda4c0935134f353fcf9802bffa6a82
dotory <kwonhyunah2@gmail.com>
2022-04-01 18:50:10 +0900

    FBV blog list page create

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from .models import Post
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
+    posts = Post.objects.all().order_by('-pk')
+
     return render(
         request,
         'blog/index.html',
+        {
+            'posts':posts,
+        }
     )

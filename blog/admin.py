67462ce8ac94e3ad26d44b31141b09aaf800ffa4
dotory <kwonhyunah2@gmail.com>
2022-05-06 19:06:37 +0900

    Post 편의 기능 제외 모두 완성

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
 # Register your models here.
 
-from .models import Post
+from .models import Post, Category
 
 admin.site.register(Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
+admin.site.register(Category, CategoryAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    preserve_fields = {'slug' : ('name', )}
+
+admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,51 @@
 from django.db import models
+from markdownx.models import MarkdownxField
+from django.contrib.auth.models import User
+
 import os
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    content = models.TextField()
+    content = MarkdownxField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+    tags = models.ManyToManyField(Tag, blank=True)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
